67e04fc835f67684d0c685fbbf79bd7f95a36767
Olle Wahlström <owlen88@gmail.com>
2019-12-10 21:24:08 +0100

    fix zindex

--- src/game/objects/boulder.js
+++ src/game/objects/boulder.js
@@ -10,16 +10,20 @@ class Boulder extends StaticObject {
     medallion.player.container.addChild(this.fogOfWarContainer);
     this.container.position.x = 0;
     this.container.position.y = 0;
+    this.container.zIndex = 0;
     this.fogOfWarContainer.position.x = 0;
     this.fogOfWarContainer.position.y = 0;
+    this.fogOfWarContainer.zIndex = 0;
   }
   stopInteract(medallion) {
     const { player, currentLevel } = medallion;
     currentLevel.addChild(this.container, this.fogOfWarContainer);
     this.container.position.x = player.position.x;
     this.container.position.y = player.position.y;
+    this.container.zIndex = player.position.y;
     this.fogOfWarContainer.position.x = player.position.x;
     this.fogOfWarContainer.position.y = player.position.y;
+    this.fogOfWarContainer.zIndex = player.position.y;
   }
 }
 
